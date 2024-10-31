fe2a7b0b5649f386c8e5c0703c8ca3e6aa47ce8d
Nathaniel Cole <nc564@cbsumehta.biohpc.cornell.edu>
2024-10-31 12:11:34 -0400

    update config.sh

--- config.sh
+++ config.sh
@@ -4,9 +4,9 @@
 IN="data"
 OUT="data/host-filtered"
 MODE="PE" # "SE" (single-end) or "PE" (paired-end) or "PE+SE" (paired-end then single-end)
-METHODS=("ALIGN-HG38" "ALIGN-T2T" "INDEX-HPRC") # any combination of "ALIGN-HG38", "ALIGN-T2T", "ALIGN-HPRC", or "INDEX-HPRC"
+METHODS="ALIGN-HPRC" # any combination of "ALIGN-HG38", "ALIGN-T2T", "ALIGN-HPRC", or "INDEX-HPRC"
 SAVE_INTERMEDIATE=1 # 0 for TRUE and 1 for FALSE
-THREADS=7
+THREADS=30
 
 # configure index filtration parameters
 METRIC="custom" # "max", "average", or "custom"
@@ -15,14 +15,14 @@ MIN_RUN_LENGTH=5
 
 # configure software and reference paths
 CONDA_ENV_NAME="human-filtration"
-MOVI_PATH="/path/to/movi-default" # path to movi-default executable
-MOVI_INDEX_PATH="ref/movi" # path to prebuilt movi_index.bin
+#MOVI_PATH="/path/to/movi-default" # path to movi-default executable
+#MOVI_INDEX_PATH="ref/movi" # path to prebuilt movi_index.bin
 MINIMAP2_PATH="$(which minimap2)" # path to minimap2 executable
 MINIMAP2_HG38_INDEX_PATH="ref/mmi/hg38.mmi" # one index
 MINIMAP2_T2T_INDEX_PATH="ref/mmi/t2t.mmi" # one index
 MINIMAP2_HPRC_INDEX_PATH="ref/mmi" # directory of indexes
 ADAPTERS="ref/known_adapters.fna"
-TMP="" # path to temporary directory for writing
+TMP="tmp" # path to temporary directory for writing
 
 # END CONFIGURATION
 
@@ -40,7 +40,6 @@ fi
 
 # define filtration map
 declare -A file_map
-file_map["FASTP"]="filter_fastp.sh"
 file_map["ALIGN-HG38"]="filter_align_hg38.sh"
 file_map["ALIGN-T2T"]="filter_align_t2t.sh"
 file_map["ALIGN-HPRC"]="filter_align_hprc.sh"
